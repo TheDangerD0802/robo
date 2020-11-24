365081a49de11f997d3aacef2d9a749e6c236620
TheDangerD <caambassdor2018@gmail.com>
2020-11-24 20:24:00 +0530

    Updates

---
+++ precache-manifest.962e24d1a6af37acdea453ef1af76794.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf50a7e59b1663a992a0f2e5fbd4724",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "31619a91bd7b87b427f5",
+    "url": "/robo/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "1bad49c5098965abea78",
+    "url": "/robo/static/css/main.b79030d1.chunk.css"
+  },
+  {
+    "revision": "31619a91bd7b87b427f5",
+    "url": "/robo/static/js/2.e1d86dab.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/robo/static/js/2.e1d86dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bad49c5098965abea78",
+    "url": "/robo/static/js/main.6734bb24.chunk.js"
+  },
+  {
+    "revision": "af839f1de40b3e92ec6c",
+    "url": "/robo/static/js/runtime-main.99a06c5d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
